5fcf47a6fc583ef0da1b715ad2e8b7ae533d8fd3
JIngyi-Liu03 <19808630452@163.com>
2026-07-21 16:29:43 +0800

    fix: prisma libc6-compat on alpine; inject JwtService into QuizModule

--- backend/src/quiz/quiz.module.ts
+++ backend/src/quiz/quiz.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { QuizService } from './quiz.service'
 import { QuizController } from './quiz.controller'
 import { PrismaModule } from '../prisma/prisma.module'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [QuizController],
   providers: [QuizService],
   exports: [QuizService],
